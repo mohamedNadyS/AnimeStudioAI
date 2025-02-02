53d90cd03ff609e884e7a9742839ddf944923de3
mohamedNadyS <mohamed0011199@gmail.com>
2025-02-02 11:56:10 +0200

    Create utils.py

---
+++ data/utils.py
@@ -0,0 +1,9 @@
+def dataset_stats(dataset):
+    """Calculate dataset statistics"""
+    # Implement dataset statistics calculation
+    pass
+
+def split_dataset(dataset, ratios=(0.8, 0.1, 0.1)):
+    """Split dataset into train/val/test"""
+    # Implement dataset splitting
+    pass
